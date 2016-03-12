cc5ac5f9805b9b871b403615279135cda6ed59a2
hootnot <f.brekeveld@gmail.com>
2016-03-12 21:18:05 +0100

    fixes in docstrings, small fixes in code, cart_delete_item() added

--- touchwood/api/endpoints/cart.py
+++ touchwood/api/endpoints/cart.py
@@ -8,59 +8,111 @@ class CartEndpointsMixin(object):
     Parameters that apply to the API url need to be passed as keyword argument.
 
     cart/items/                 GET     list all items in cart
-    cart/items/:id/             GET     list specific item in cart
+    cart/items/:cid/            GET     list specific item in cart
 
     cart/items                  POST    add an item to the cart
                                         body: { "id" : <article-id>,
                                                 "csrfmiddlewaretoken" : <tok> }
+    cart/items                  DELETE  delete all items
+    cart/items/:cid/            DELETE  delete item with cart-id cid
     """
 
     def cart_get_items(self, **params):
         """cart_get_items - get a list of stored cart items.
 
-        API-endpoints
+        API-endpoint
         -------------
         cart/items
 
         returns :
-            a dictionary containing article information of all items
+            a dictionary containing article information of all items.
         """
         endpoint = "cart/items"
 
         return self.request(endpoint, params=params)
 
-    def cart_get_item(self, item, **params):
+    def cart_get_item(self, cid, **params):
         """cart_get_item - get a specific item from cart.
 
-        API-endpoints
+        API-endpoint
         -------------
         cart/item
 
-        returns :
-            a dictionary containing article information
-        """
-        endpoint = "cart/item"
-        if not item:
-            raise ValueError("item = " + str(item))
+        lookup the specified item in the cart by it's cart-id (cid) and return
+        it's related information.
 
-        endpoint += "{}/".format(item)
+        If cid is not specified a ValueError is raised.
+
+        returns :
+            a dictionary containing article information of the specified item.
+        """
+        endpoint = "cart/items"
+        if not cid:
+            raise ValueError("cid = " + str(cid))
+
+        endpoint += "/{}".format(cid)
 
         return self.request(endpoint, params=params)
 
-    def cart_add_item(self, item, **params):
+    def cart_add_item(self, **params):
         """cart_add_item - add item to cart.
+
+        API-endpoint
+        -------------
+        cart/items
+
+        add an item to the cart. This is done by passing the id of the article
+        in a JSON formatted body to the server:
+
+            {
+              "id" : "N901",
+              "csrfmiddlewaretoken" : "Yy5qSxbT4eAJnFnQbUR3nh75rDN4Riwm"
+            }
+
+        The server queries it's resources for the article and adds it to the
+        cart.
+
+        returns :
+            a dictionary containing cart-id (cid) for the new article:
+
+            {
+               "cid": <cid>
+            }
+
+        """
+        endpoint = "cart/items"
+
+        return self.request(endpoint, "POST", params=params)
+
+    def cart_delete_item(self, item=None, **params):
+        """cart_delete_item - delete item from cart.
 
         API-endpoints
         -------------
         cart/items
 
         returns :
-            a dictionary containing cart-id (cid) for the new article
+            in case the item is specified with a cid,
+            a dictionary containing cart-id (cid) for the article to delete:
+
+              {
+                "cid": <cid>
+              }
+
+            HTTP status 200 if success
+            HTTP status 404 if not found
+
+            in case there is no item specified, all items will be deleted
+            and a dictionary:
+
+              {
+                "deleted": <num>
+              }
+
+            is returned. In the latter case always with return HTTP status 200.
         """
-        endpoint = "cart/item"
-        if not item:
-            raise ValueError("item = " + str(item))
+        endpoint = "cart/items"
+        if item:
+            endpoint += "/{}".format(item)
 
-        endpoint += "{}/".format(item)
-
-        return self.request(endpoint, "POST", params=params)
+        return self.request(endpoint, "DELETE", params=params)
